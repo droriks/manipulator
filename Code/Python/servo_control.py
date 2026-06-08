d7111b7f9c41f621ffb18fdc1ca4de38b64fd9f4
droriks <dkosowskysachs@gmail.com>
2026-06-08 10:33:18 -0400

    test code final

--- Code/Python/servo_control.py
+++ Code/Python/servo_control.py
@@ -1,17 +1,27 @@
 #Import Libraries\
 from gpiozero import AngularServo
 from time import sleep
-servo = AngularServo(14, min_angle = 0, max_angle = 270, min_pulse_width = .5/1000, max_pulse_width = 2.5/1000)
+servo = AngularServo(14, min_angle = 0, max_angle = 180, min_pulse_width = .5/1000, max_pulse_width = 2.5/1000)
+
+def main():
+    try:
+        while True:
+            angle = int(input("Enter angle (0 to 180): "))
+            set_angle(angle)
+
+    except KeyboardInterrupt:
+        print("Program stopped by user")
+
+
+
+
+
 
 def set_angle(angle):
     servo.angle = angle
     sleep(1)
 
-try:
-    while True:
-        angle = int(input("Enter angle (0 to 270): "))
-        set_angle(angle)
+main()
 
-except KeyboardInterrupt:
-    print("Program stopped by user")
+
 
