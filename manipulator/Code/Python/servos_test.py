e9ca0fdb6e489348e8ee448983169d657834002c
drorks <drorks@terpmail.umd.edu>
2026-06-17 09:26:08 -0400

    timer to hop in

--- manipulator/Code/Python/servos_test.py
+++ manipulator/Code/Python/servos_test.py
@@ -12,21 +12,24 @@ pca.frequency = 50
 switch = Button(17, pull_up=True)
 
 def angle_to_duty(angle):
-    pulse_us = 2500 - (angle + 90) / 180 * (2500 - 500)
+    pulse_us = 500 + (angle) / 180 * (2500 - 500)
     return int(pulse_us / 20000 * 65535)
 
 def set_angle(channel, angle):
     pca.channels[channel].duty_cycle = angle_to_duty(angle)
 
 # --- Test ---
-angle = 0.0
+angle = 90.0
 channel = 0  # change to whichever channel you're testing
+
+set_angle(channel, angle)
+sleep(3)
 
 print("Starting — driving down until switch hit")
 
 try:
     while not switch.is_pressed:
-        angle -= 0.5
+        angle += 0.5
         print(f"Angle: {angle:.1f}")
         set_angle(channel, angle)
         sleep(0.05)
