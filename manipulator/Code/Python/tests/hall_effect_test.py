ae0975ba0b92d50613cfa4a646dc39137efc94e6
drorks <drorks@terpmail.umd.edu>
2026-07-15 16:39:06 -0400

    hall effect again

--- manipulator/Code/Python/tests/hall_effect_test.py
+++ manipulator/Code/Python/tests/hall_effect_test.py
@@ -1,14 +1,9 @@
-import time
 from gpiozero import DigitalInputDevice
+from signal import pause
 
+sensor = DigitalInputDevice(16, pull_up=True)
 
-sensor = DigitalInputDevice(pin=16, pull_up=True)
+sensor.when_activated = lambda: print("magnet detected")
+sensor.when_deactivated = lambda: print("magnet removed")
 
-while True:
-    if magnet_detected:
-        print("magnet detected")
-
-    if magnet_removed:
-        print("magnet removed")
-    time.sleep(.5)
-
+pause()  # keep the script alive; gpiozero fires callbacks in the background
